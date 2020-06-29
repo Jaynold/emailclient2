43641658560567da4c8592d7d66d0ee5afe6f032
Abhay Shiravanthe <ashiro@teknorix.com>
2020-06-29 15:29:09 +0530

    bug fixes

--- server/index.js
+++ server/index.js
@@ -18,7 +18,6 @@ app.post('/login', (req, res) => {
             message: null,
             token: "hfjhfg2h3jf423f4hj3f24g234v23jhf4j23fbu4jf23j423jf4hj23b4jnfdsf8td7s8fnt8dsfmsdf"
         })
-        return next();
     } else {
         res.status(401).send({
             error: 401,
