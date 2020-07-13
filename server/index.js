7a5b91f6639e345dc3e323311bd3a68578ef8f52
Abhay Shiravanthe <ashiro@teknorix.com>
2020-07-13 10:56:41 +0530

    Cors added to prevent browser response error

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 var express = require('express');
+var cors = require('cors')
 var bodyParser = require('body-parser')
 
 var app = express();
+
+app.use(cors())
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
